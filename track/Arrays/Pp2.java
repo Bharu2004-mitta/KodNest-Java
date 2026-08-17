8ec12f8d06f0830ba6d182768f746a6eedf25972
Mittaveedi Bhargav <bhargavmittaveedi2004@gmail.com>
2026-08-17 15:34:39 +0530

    Arrays Pp2

---
+++ track/Arrays/Pp2.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+
+public class Pp2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+        System.out.println("Enter numbers:");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int num : arr) {
+            System.out.println(num);
+        }
+    }
+}
